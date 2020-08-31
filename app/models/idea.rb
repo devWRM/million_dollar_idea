1259b28803ea0e63cf736a78cccf0d2f77f72374
devWRM <mcwen@comcast.net>
2020-08-31 16:26:44 -0400

    Added missing colon to :goals on line 4

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
   has_many :goals						
-  has_many :categories, through: goals			
+  has_many :categories, through: :goals			
   has_many :supports
   has_many :resources, through: :supports
 
