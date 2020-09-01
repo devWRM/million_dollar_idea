06f1111feeb2efc6148ffeeb15c27a7b42274c09
devWRM <mcwen@comcast.net>
2020-08-31 20:01:19 -0400

    Built strong params under private for user_params & built the create action using user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,26 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
 
     end
 
 
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+
+    
 
 end
