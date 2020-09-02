9a79e3369627845e763644b63dc2498d4e5a9fd0
devWRM <mcwen@comcast.net>
2020-09-02 09:44:02 -0400

    Finished building the create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,37 @@
 class SessionsController < ApplicationController
     def home
+
+        # Welcome
+    end
+
+    def new
+        # Login
+    end
+
+    def create
+     
+        user = User.find_by(email: params[:user][:email])
+
+        if user && user.authenticate(params[:user][:email])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+            # redirect_to "/users/#{user.id}"
+        else
+            flash[:message] = "Received invalid login information. Please try again."
+            redirect_to login_path
+        end
     end
 
     def destroy
+        # Logout
         session.clear
         redirect_to root_path
     end
+
+    
+
+
+
+
 end
 
