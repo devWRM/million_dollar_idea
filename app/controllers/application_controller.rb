4b16df5b3e83bbb398fff5e7df06a02a607cde61
devWRM <mcwen@comcast.net>
2020-09-02 15:11:38 -0400

    Added 3 helper methods: current_user, logged_in?, redirect_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+    
+
+
+
+
+    
+
+        def current_user
+            @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+        end
+
+        def logged_in?
+            !!current_user
+            # !!session[:user_id]
+        end
+
+        def redirect_if_not_logged_in
+            redirect_to '/' if !logged_in?
+        end
+
+        # def owner?(idea)
+        #     idea.user == current_user
+        # end
+
+    
+
+
+
 end
