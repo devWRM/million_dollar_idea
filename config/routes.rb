ecfb4cfc6b82c2445271d9ac1deeeba7694bad9f
devWRM <mcwen@comcast.net>
2020-09-05 13:51:14 -0400

    Added Nested Routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,50 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
 
-
   # write custom routes above
 
-  resources :resources
-  resources :supports
-  resources :categories
-  resources :goals
-  resources :ideas
-  # resources :ideas, only: [:show]
-  # resources :ideas, to: "ideas#show"
+  
+  resources :ideas do
+    resources :supports, only: [:new, :create, :index]    # <<= All a specific idea’s supports
+    resources :goals, only: [:new, :create, :index]       # <<= All a specific idea’s goals
+    # resources :categories, only: [:new, :create, :index]
+    # resources :resources, only: [:new, :create, :index]
+  end
+
+
+  # Reduced to just the index
+  resources :users do
+    resources :ideas, only: [:index]  # :new, :create     <<= All a specific user’s ideas
+    resources :supports, only: [:index] # :new, :create   <<= All a specific user’s supports
+    resources :goals, only: [:index] #:new, :create       <<= All a specific user’s goals
+  end
+
+
+  resources :ideas  
   resources :users
+
+  resources :supports
+  resources :goals
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
+# resources :resources do
+  #   resources :supports, only: [:new, :create, :index]
+  #   resources :ideas, only: [:new, :create, :index]
+  # end
+  
+  # resources :categories do
+  #   resources :goals, only: [:new, :create, :index]
+  #   resources :ideas, only: [:new, :create, :index]
+  # end
+  
+
+
+
+
