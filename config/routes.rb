4242fab14eca7fb46f021ee5c0a43ab3105f0994
devWRM <mcwen@comcast.net>
2020-09-05 20:41:02 -0400

    Changed users & ideas nesting to include resources and categories instead of supports & goals

--- config/routes.rb
+++ config/routes.rb
@@ -15,26 +15,31 @@ Rails.application.routes.draw do
 
   
   resources :ideas do
-    resources :supports, only: [:new, :create, :index]    # <<= All a specific idea’s supports
-    resources :goals, only: [:new, :create, :index]       # <<= All a specific idea’s goals
-    # resources :categories, only: [:new, :create, :index]
-    # resources :resources, only: [:new, :create, :index]
+    # resources :supports, only: [:new, :create, :index]    # <<= All a specific idea’s supports
+    # resources :goals, only: [:new, :create, :index]       # <<= All a specific idea’s goals
+    resources :resources, only: [:new, :create, :index]
+    resources :categories, only: [:new, :create, :index]
   end
 
 
   # Reduced to just the index
   resources :users do
     resources :ideas, only: [:index]  # :new, :create     <<= All a specific user’s ideas
-    resources :supports, only: [:index] # :new, :create   <<= All a specific user’s supports
-    resources :goals, only: [:index] #:new, :create       <<= All a specific user’s goals
+
+    resources :resources, only: [:index]
+    resources :categories, only: [:index]
+    # resources :supports, only: [:index] # :new, :create   <<= All a specific user’s supports
+    # resources :goals, only: [:index] #:new, :create       <<= All a specific user’s goals
   end
 
-
-  resources :users    # ?? May not need this route
-  resources :ideas    # ?? May not need this route
-
+  resources :resources
+  resources :categories
   resources :supports
   resources :goals
+
+
+  # resources :users    # ?? May not need this route
+  # resources :ideas    # ?? May not need this route
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
