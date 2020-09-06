18c6c3c765ded9ce8790fd9b503e21bab60bfc39
devWRM <mcwen@comcast.net>
2020-09-05 21:31:38 -0400

    Added index, new, create actions

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,18 @@
 class ResourcesController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+
+
 end
