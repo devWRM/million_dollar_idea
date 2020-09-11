420e5afc55b5c09c3e0c66cdc619d410026a728a
devWRM <mcwen@comcast.net>
2020-09-11 05:48:14 -0400

    Added uniqueness to validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
     has_secure_password  # access to authenticate & validations
 
-    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true, presence: true
 end
