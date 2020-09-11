e836db058c6207fc1cc1788b2f607ddeba20198b
devWRM <mcwen@comcast.net>
2020-09-11 09:36:30 -0400

    Added the dependent destroy command to make sure all children of ideas are destroyed when the idea parent is destroyed

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,11 +1,11 @@
 class Idea < ApplicationRecord
   belongs_to :user
-  has_many :goals						
+  has_many :goals, :dependent => :destroy					
   has_many :categories, through: :goals			
-  has_many :supports
+  has_many :supports, :dependent => :destroy
   has_many :supported_resources, through: :supports, source: :resource
 
-  has_many :resources   # To enable:  resource.idea
+  has_many :resources, :dependent => :destroy   # To enable:  resource.idea
 
   validates :title, :inspiration, :plan, :budgeted_dollars, :spent_dollars, presence: true
   scope :alpha, -> { order(:title) }
