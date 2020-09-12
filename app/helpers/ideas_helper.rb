b58874dbc9cb6814a8558d9471adf6df5d8cde81
devWRM <mcwen@comcast.net>
2020-09-12 10:39:49 -0400

    Commented out attempt to write a helper method to extract out a heading because it was complicated by a link

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,27 @@
 module IdeasHelper
+
+    # <h2><%= index_heading %></h2>
+
+
+
+    # def index_heading
+
+    #     if @ideas.blank?
+
+    #         "You currently have no ideas, #{@user.username}. Create an idea <a href='/ideas/new'>here</a>"     , new_idea_path%></h2>
+
+    #     else
+
+    #         "All Ideas"
+    #         # if @user
+    #         #     by<% if current_user == @user %> you<% end %>, <%= @user.username %>
+    #         # end
+
+    #     end
+
+    # end
+
+
+
+
 end
