f5124e84828c8001ce39a3e31b0573a5a5246666
devWRM <mcwen@comcast.net>
2020-09-12 10:37:27 -0400

    Created helper method to extract out the heading of the resources index

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,11 @@
 module ResourcesHelper
+
+    def resource_index_heading
+        if @idea 
+            "All Resources for #{@idea.title}"
+        else
+            "All Resources"
+        end
+    end
+
 end
