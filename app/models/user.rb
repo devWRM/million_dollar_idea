ad6b87f1de89681aff60511d85ba5d70b36849c1
devWRM <mcwen@comcast.net>
2020-09-13 09:50:53 -0400

    Restructured association relationships which added 2 has many through associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
     has_many :ideas
 
+    has_many :categories, through: :ideas
+    has_many :resources, through: :ideas
+
+
+
+
     has_secure_password  # access to authenticate & validations
 
     validates :username, :email, uniqueness: true, presence: true
