15f4896024c223061cc6b5b1219ccc31f9f248e6
devWRM <mcwen@comcast.net>
2020-09-13 17:02:41 -0400

    Reversed the nesting of ideas and categories to show ideas nested in categories and not the reverse

--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,14 @@ Rails.application.routes.draw do
   resources :ideas do
     # resources :supports, only: [:new, :create, :index]    # <<= All a specific idea’s supports
     # resources :goals, only: [:new, :create, :index]       # <<= All a specific idea’s goals
-    resources :categories, only: [:index]
+    # resources :categories, only: [:index]
     resources :resources, only: [:new, :create, :index]
   end
 
-  resources :resources
-  resources :categories
-
+  resources :categories do
+    resources :ideas, only: [:index]
+  end
+  
   # Reduced to just the index
   resources :users do
     resources :ideas, only: [:index]  # :new, :create     <<= All a specific user’s ideas
@@ -36,8 +37,9 @@ Rails.application.routes.draw do
     # resources :supports, only: [:index] # :new, :create   <<= All a specific user’s supports
     # resources :goals, only: [:index] #:new, :create       <<= All a specific user’s goals
   end
-
   
+  
+  resources :resources
   resources :supports
   # resources :goals
 
