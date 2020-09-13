71bae1a014491f60e56005fa3c637e7777dd1b38
devWRM <mcwen@comcast.net>
2020-09-13 18:07:48 -0400

    Built the index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,15 @@
 class CategoriesController < ApplicationController
+
+    def index
+
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @categories = @user.categories
+        else
+            @error = "That user doesn't exist" if params[:user_id]
+            @categories = Category.all
+        end
+
+    end
+
+
 end
