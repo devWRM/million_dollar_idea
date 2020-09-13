270836cda16c424b2c974378218004ee17302f96
devWRM <mcwen@comcast.net>
2020-09-13 15:50:54 -0400

    Added accepts class methods so categories data can be access from the new ideas form

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -7,6 +7,11 @@ class Idea < ApplicationRecord
 
   has_many :resources, :dependent => :destroy   # To enable:  resource.idea
 
+
+  accepts_nested_attributes_for :category
+  
+
   validates :title, :inspiration, :plan, :budgeted_dollars, :spent_dollars, presence: true
+  
   scope :alpha, -> { order(:title) }
 end
