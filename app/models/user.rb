714e2bbae1bef1ed02c5f638a65db57c00889864
devWRM <mcwen@comcast.net>
2020-09-17 07:08:03 -0400

    Added commented out validation alternatives

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
     has_secure_password  # access to authenticate & validations
 
     validates :username, :email, uniqueness: true, presence: true
+
+    # validates :username, :presence => true, :message => ""
+    # validates :username, :uniqueness => true, :message => ""
+    # validates :email, :uniqueness => true, message => ""
+    # validates :email, :presence => true, message => ""
 end
