613869a6f3cf472fee8bbee65a12ed0e0b93c6b7
devWRM <mcwen@comcast.net>
2020-09-17 17:17:19 -0400

    Added .uniq so categories would not repeat

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
+    before_action :redirect_if_not_logged_in
 
     def index
 
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
-            @categories = @user.categories
+            @categories = @user.categories.uniq
         else
             @error = "That user doesn't exist" if params[:user_id]
             @categories = Category.all
