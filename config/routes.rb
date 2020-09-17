787d3e0820a21192c1fed425cd6f5e2a623ce51f
devWRM <mcwen@comcast.net>
2020-09-17 18:59:57 -0400

    Deleted excessive commented out code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-# rails routes
+
 Rails.application.routes.draw do
   root "sessions#home"
 
@@ -19,9 +19,7 @@ Rails.application.routes.draw do
   # write custom routes above
 
   resources :ideas do
-    # resources :supports, only: [:new, :create, :index]    # <<= All a specific idea’s supports
-    # resources :goals, only: [:new, :create, :index]       # <<= All a specific idea’s goals
-    # resources :categories, only: [:index]
+    
     resources :resources, only: [:new, :create, :index]
   end
 
@@ -35,19 +33,12 @@ Rails.application.routes.draw do
     resources :ideas, only: [:index]  # :new, :create     <<= All a specific user’s ideas
     resources :resources, only: [:index]
     resources :categories, only: [:index]
-    # resources :supports, only: [:index] # :new, :create   <<= All a specific user’s supports
-    # resources :goals, only: [:index] #:new, :create       <<= All a specific user’s goals
   end
   
   
   resources :resources
   resources :supports
-  # resources :goals
-
-
-  # resources :users    # ?? May not need this route
-  # resources :ideas    # ?? May not need this route
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
@@ -56,16 +47,6 @@ end
 
 
 
-# resources :resources do
-  #   resources :supports, only: [:new, :create, :index]
-  #   resources :ideas, only: [:new, :create, :index]
-  # end
-  
-  # resources :categories do
-  #   resources :goals, only: [:new, :create, :index]
-  #   resources :ideas, only: [:new, :create, :index]
-  # end
-  
 
 
 
