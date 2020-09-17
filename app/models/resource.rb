c445d429907547cb1a002d9bef97af572e7fb2f5
devWRM <mcwen@comcast.net>
2020-09-17 10:46:02 -0400

    Commented out attributes method during trial testing to get the categories selector to work, so no changes

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -5,4 +5,11 @@ class Resource < ApplicationRecord
     belongs_to :idea     # To enable:     resource.idea
 
     validates :source, presence: true
+
+
+    # def idea_attributes=(attr)
+    #     self.idea = Idea.find_or_create_by(attr) if !attr[:title].blank?
+    # end
+    
+
 end
